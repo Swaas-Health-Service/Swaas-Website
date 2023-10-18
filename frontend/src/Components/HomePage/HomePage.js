6d18121bad5d3c563df4b4b4f0ef4b48517b8e56
rupammahato <rupammahato@kgpian.iitkgp.ac.in>
2023-10-18 23:55:08 +0530

    minor changes

--- frontend/src/Components/HomePage/HomePage.js
+++ frontend/src/Components/HomePage/HomePage.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import "./HomePage.css";
 import { Link } from 'react-router-dom';
-// import {Link} from "react-router-dom";
 const HomePage = () => {
   return (
     <>
